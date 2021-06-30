a9f3acdfb1b92120f54c3b822741ea93d9352f36
jayesh thanvi <jayeshmemorres>
2021-06-30 20:08:22 +0530

    banner complete

--- dist/script.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var hamburger = document.getElementById('hamburger');
-var menu = document.getElementById('menu-list');
-hamburger.addEventListener('click', function () {
-  menu.classList.toggle('show');
-});
--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+var i = 0;
+var images = [];
+var slideTime = 3000; // 3 seconds
+images[0] = "banner2.jpg";
+images[1] = 'banner.jpg';
+images[2] = 'banner3.Jpg';
+
+function changePicture() {
+    document.getElementById("background").style.backgroundImage = "url(" + images[i] + ")";
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout(changePicture, slideTime);
+}
+window.onload = changePicture
+
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu-list');
+
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('show');
 })
